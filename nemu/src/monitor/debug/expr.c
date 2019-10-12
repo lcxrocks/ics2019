8cb4b8a9bd058e8ed0999dbfd4facc524c9c1f85
tracer-ics2019 <tracer@njuics.org>
2019-10-12 16:49:07 +0800

    >  run 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  16:49:07 up 15:51,  3 users,  load average: 0.74, 0.75, 0.60 3fa72b9c1c3fba2cc7a2de488c72c618a114e58c

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -5,12 +5,13 @@
  */
 #include <sys/types.h>
 #include <regex.h>
-
+#include <string.h>
+#include <stdlib.h>
+uint32_t isa_reg_str2val(char *, bool *);
 enum {
-  TK_NOTYPE = 256, TK_EQ
-
+  TK_NOTYPE = 256, TK_EQ,  NUM, REGS, HEX,TK_NEQ, TK_ANDL,TK_DECONF,TK_ORL
   /* TODO: Add more token types */
-
+ //So the initial value for space is 256, incrementing 1 each element afterwards.
 };
 
 static struct rule {
@@ -24,11 +25,22 @@ static struct rule {
 
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
-  {"==", TK_EQ}         // equal
+  {"==", TK_EQ},        // equal
+  {"!=", TK_NEQ},       //not-equal
+  {"/", '/'},           // divide
+  {"\\*", '*'},           // multiply or DECONP
+  {"\\-", '-'},           //minus
+  {"[&][&]", TK_ANDL},           //logic-and
+  {"[|][|]", TK_ORL},            //logic-or
+  {"\\(", '('},           //left parenthesis
+  {"\\)", ')'},           //right parenthesis
+  {"0x[0-9,a-f,A-F]+", HEX},             //hex-numbers*/
+  {"[0-9]+", NUM},             //numbers
+  {"[$][a-z]+", REGS},          //regs
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
-
+//number of regex
 static regex_t re[NR_REGEX] = {};
 
 /* Rules are used for many times.
@@ -53,40 +65,67 @@ typedef struct token {
   char str[32];
 } Token;
 
-static Token tokens[32] __attribute__((used)) = {};
+static Token tokens[1024] __attribute__((used)) = {};
 static int nr_token __attribute__((used))  = 0;
 
 static bool make_token(char *e) {
   int position = 0;
   int i;
   regmatch_t pmatch;
-
+  memset(tokens,'\0',sizeof(tokens));
   nr_token = 0;
-
-  while (e[position] != '\0') {
+  while (e[position] != '\0' && e[position] != '\n') {
     /* Try all rules one by one. */
     for (i = 0; i < NR_REGEX; i ++) {
       if (regexec(&re[i], e + position, 1, &pmatch, 0) == 0 && pmatch.rm_so == 0) {
-        char *substr_start = e + position;
+        char *substr_start = e + position; // pos=substr_start=pmatch.so
         int substr_len = pmatch.rm_eo;
 
-        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
-            i, rules[i].regex, position, substr_len, substr_len, substr_start);
+       // Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+         //   i, rules[i].regex, position, substr_len, substr_len, substr_start);
+           //pmatch.so:regex's start;
+           //pmatch.eo:regex's end;        
         position += substr_len;
-
         /* TODO: Now a new token is recognized with rules[i]. Add codes
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
-
+        //printf("rules[%d].token_type: %d\n", i, rules[i].token_type);
         switch (rules[i].token_type) {
-          default: TODO();
+          case TK_NOTYPE: break; //do nothing
+          case '+':
+          case '-':
+          case '*':
+          case '/':
+          case '(':
+          case ')':
+          case TK_NEQ: 
+          case TK_EQ:
+          case TK_ANDL:
+          case TK_ORL:
+                    tokens[nr_token].type=rules[i].token_type;
+                    nr_token++;
+                    break; 
+          case NUM:
+          
+          case HEX: tokens[nr_token].type=rules[i].token_type;
+                    strncpy(tokens[nr_token].str, substr_start, substr_len);
+                    nr_token++;
+                    break;
+          case REGS:tokens[nr_token].type=rules[i].token_type;
+                    strncpy(tokens[nr_token].str, substr_start+1, substr_len-1);
+                    nr_token++;
+                    break; //skip '$'
+          
+          default: tokens[nr_token].type=rules[i].token_type;
+                    strncpy(tokens[nr_token].str,substr_start,substr_len);
+                    nr_token++;
+                    break;
         }
-
+    
         break;
       }
     }
-
     if (i == NR_REGEX) {
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
       return false;
@@ -96,14 +135,103 @@ static bool make_token(char *e) {
   return true;
 }
 
+bool check_parentheses(int p, int q){
+int left=0;
+if(tokens[p].type=='(' && tokens[q].type==')')   
+ {       
+    for(int i=p; i<=q; i++){
+            if(tokens[i].type=='(') left++;
+            if(tokens[i].type==')') left--;
+            if(left==0 && i!=q) return false; //found right par in the middle;
+        }
+    if(left==0) return true;
+    else return false;
+    }
+   // printf("checking parentheses %d, %d\n", p ,q );
+    
+    else return false;
+    /********
+    if(tokens[p].type=='(' && tokens[q].type==')') return true;
+    else return false;
+    //The simplest version of checking parentheses****/
+ }
+
+int eval(int p, int q){
+    //printf("calling eval(%d, %d)\n", p, q);
+    if(p>q) {
+        printf("Left boundary exceeds the right one? What are you even thinking?(p(%d)>q(%d))\n",p,q);
+        assert(0);
+    }
+    else if(p==q){
+        char *stop;
+        bool success=true;
+        if(tokens[p].type==NUM) 
+            return atoi(tokens[p].str); 
+        else if(tokens[p].type==HEX) 
+            return strtoul(tokens[p].str,&stop, 16);
+        else if(tokens[p].type==REGS)
+            return isa_reg_str2val(tokens[p].str, &success);
+        else{
+        printf("I don't think this expr stands for anything.(p(%d)=q(%d))\n",p,q);
+        assert(0);
+        }
+    }
+    else if(check_parentheses(p,q)) {
+      //printf("calling inside parentheses eval(%d,%d)\n", p+1,q-1); 
+      return eval(p+1,q-1);
+      }
+    else{
+        int op=-1;
+        int leftp=0;
+        bool pm=false; //plus or minus
+        for(int i=p; i<=q; i++){
+            if(tokens[i].type=='(') leftp++;
+            if(tokens[i].type==')') leftp--;
+            if(leftp==0){
+                if(tokens[i].type=='+'||tokens[i].type=='-') {op=i;pm=true;} 
+                else if(((tokens[i].type=='*'||tokens[i].type=='/'))&&(pm==false)) op=i;
+                else if(tokens[i].type==TK_EQ || tokens[i].type==TK_NEQ || tokens[i].type==TK_ORL || tokens[i].type==TK_ANDL) op=i;
+            }
+        }
+        //printf("op: %d\n",op);
+        int val1=0;
+        int val2=0;
+        if(op){
+        //printf("calling forward eval(%d,%d)\n",p,op-1);
+          val1=eval(p,op-1);
+        //printf("calling backward eval(%d,%d)\n",op+1,q);
+          val2=eval(op+1,q);
+        }
+        switch(tokens[op].type){
+            case '+': return val1+val2;
+            case '-': return val1-val2;
+            case '*': 
+                      if(op==p){
+                          char *end;
+                          vaddr_t addr=strtol(tokens[op+1].str, &end, 16);
+                          return vaddr_read(addr,4);
+                      }
+                      else
+                          return val1*val2;
+
+            case '/': return val1/val2;
+            case TK_ORL: return val1 || val2;
+            case TK_ANDL: return val1 && val2;
+            case TK_EQ: return val1==val2;
+            case TK_NEQ: return val1!=val2;
+            default : printf("switch glitch at line : %d",__LINE__); assert(0);
+            //Simplest version of eval
+        }
+    }
+}
 uint32_t expr(char *e, bool *success) {
   if (!make_token(e)) {
     *success = false;
     return 0;
   }
-
+  //printf("nr_token is :%d\n",nr_token);
   /* TODO: Insert codes to evaluate the expression. */
-  TODO();
-
-  return 0;
+  uint32_t nrtoken=nr_token-1; 
+  //printf("nrtoken is %d\n",nrtoken);
+  return  eval(0,nrtoken);
 }
--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -8,7 +8,12 @@
 #include <readline/history.h>
 
 void cpu_exec(uint64_t);
-
+void isa_reg_display();
+uint32_t expr();
+uint32_t vaddr_read();
+WP* new_wp();
+void free_wp();
+void watchpoint_info();
 /* We use the `readline' library to provide more flexibility to read from stdin. */
 static char* rl_gets() {
   static char *line_read = NULL;
@@ -32,12 +37,73 @@ static int cmd_c(char *args) {
   return 0;
 }
 
+static int cmd_si(char *args){
+  /* extract the first argument */
+  char *arg = strtok(NULL, " ");
+  if (arg==NULL) cpu_exec(1);
+  else{
+     printf("arg: %s\n",arg);
+     int steps = atoi(arg);
+     cpu_exec(steps);
+}    
+   printf("done");
+   return 0;
+}
+
+static int cmd_info (char *args){
+  /* extract the first argument */
+  char *arg = strtok(NULL, " ");
+  if(strcmp(arg, "r")==0) isa_reg_display();
+  else if(strcmp(arg, "w")==0) watchpoint_info();
+  else 
+  {
+    printf("Invaild operation");
+    return -1;
+  }
+  return 0;
+}
+
 static int cmd_q(char *args) {
   return -1;
 }
 
-static int cmd_help(char *args);
+static int cmd_x(char *args) {
+ /* extract the first argument */
+  char *arg = strtok(NULL, " ");
+  if(arg==NULL) {
+  printf("Please tell me how many times I should excute.");
+  return -1;
+  }
+  int n = atoi(arg);
+  
+  char *EXPR = strtok(NULL, " ");
+  if(EXPR==NULL){
+  printf("Show me the location please");
+  return -1;
+  }
+  if(strtok(NULL, " ")){
+  printf("Too many parameters!");
+  return -1;
+  }
 
+  char *str;
+  vaddr_t addr=strtol(EXPR, &str, 16); //convert to hex
+  for(int i=0; i<n; i++){
+    uint32_t data=vaddr_read(addr+4*i,4);
+    printf("0x%08x :", addr+4*i);
+    for(int j=0; j<4; j++){
+        printf("0x%02x  ", data &0xff);
+        data>>=8;
+    }
+    printf("\n");
+  }
+  return 0;
+}
+
+static int cmd_help(char *args);
+static int cmd_p(char *args); 
+static int cmd_w(char *args);
+static int cmd_d(char *args);
 static struct {
   char *name;
   char *description;
@@ -46,12 +112,46 @@ static struct {
   { "help", "Display informations about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
+  { "si", "single-step execution N times", cmd_si},
+  { "info", "'info r' print out the value of all register/'info w' watchpoint", cmd_info},
+  { "x", "print out the value(s) of given address", cmd_x},
+  { "p", "print expr's val and dereference and print the register", cmd_p},
+  { "w", "set watchpoint at expr", cmd_w},
+  { "d", "delete watchpoint [i]", cmd_d},
   /* TODO: Add more commands */
-
 };
 
 #define NR_CMD (sizeof(cmd_table) / sizeof(cmd_table[0]))
+
+static int cmd_w(char *args) {
+  char *arg = strtok(NULL, " ");
+  if(arg==NULL){
+    printf("Give me any expr to watch plz.\n");
+    assert(0);
+  }
+  WP* wp=new_wp();
+  bool success=true;
+  sscanf(arg, "%s", wp->expr);
+  wp->old_value = expr(args, &success);
+  printf("Watchpoint[%d] Successfully set at expr: %s with its original val: %d\n", wp->NO,wp->expr, wp->old_value);
+  return 0;
+}
+
+static int cmd_d(char *args){
+  //d 1 or d 1-2 or d all
+  char *arg = strtok(NULL, " ");
+  int num = atoi(arg);
+  free_wp(num);
+  printf("Deleted watchpoint %d\n",num);
+  return 0;
+}
+static int cmd_p(char *args) {
+   bool lcxnb=true;
+   char *arg = strtok(NULL, " ");
+   uint32_t expr_cpt=expr(arg, &lcxnb);
+   printf("%u\n",expr_cpt);
+   return 0;
+}
 
 static int cmd_help(char *args) {
   /* extract the first argument */
