af707f4fc6423518dc41ee30d4fa25fa9878ee18
tracer-ics2019 <tracer@njuics.org>
2019-10-18 00:33:31 +0800

    >  compile 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  00:33:31 up 1 day, 23:08,  4 users,  load average: 0.31, 0.20, 0.08 a934d177e0a703626bbf1f3c7b297d99bd9f3bd

--- nemu/src/isa/x86/include/isa/rtl.h
+++ nemu/src/isa/x86/include/isa/rtl.h
@@ -34,7 +34,7 @@ static inline void rtl_push(const rtlreg_t* src1) {
 static inline void rtl_pop(rtlreg_t* dest) {
   // dest <- M[esp]
   // esp <- esp + 4
-  rtl_sm(dest, &cpu.esp, 4);
+  rtl_lm(dest, &cpu.esp, 4);
   cpu.esp += 4;
 }
 
