f71834524ae7156dcf4804acbaf793fd8171eb79
tracer-ics2019 <tracer@njuics.org>
2019-10-18 23:00:19 +0800

    >  compile 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  23:00:19 up 2 days,  3:19,  4 users,  load average: 0.39, 0.32, 0.20 30a0d5419dab5f955eb72e1b87d47b9c6736b23c

--- nemu/src/isa/x86/exec/all-instr.h
+++ nemu/src/isa/x86/exec/all-instr.h
@@ -1,11 +1,16 @@
 #include "cpu/exec.h"
-
+//data-mov
 make_EHelper(mov);
+make_EHelper(lea);
+
 make_EHelper(call);
 make_EHelper(push);
 make_EHelper(sub);
 make_EHelper(xor);
 make_EHelper(ret);
+
+
+
 make_EHelper(operand_size);
 make_EHelper(nop);
 make_EHelper(inv);
