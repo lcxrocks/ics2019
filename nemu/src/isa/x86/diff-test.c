a571adaa7f3d4af422528aaa4f87e19d11b49217
tracer-ics2019 <tracer@njuics.org>
2019-10-24 22:51:49 +0800

    >  compile 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  22:51:49 up 3 days, 11:15,  7 users,  load average: 0.38, 0.42, 0.29 274c7e221db918cfb500721bcb6ac228434779e

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -3,6 +3,18 @@
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   bool equal = memcmp(&cpu, ref_r, DIFFTEST_REG_SIZE);
+  /*
+  printf("eax_diff 0x%8x %8u\n", ref_r->eax);
+  printf("ecx 0x%8x %8u\n", ref_r->ecx);
+  printf("edx 0x%8x %8u\n", ref_r->edx);
+  printf("ebx 0x%8x %8u\n", ref_r->ebx);
+  printf("esp 0x%8x %8u\n", ref_r->esp);
+  printf("ebp 0x%8x %8u\n", ref_r->ebp);
+  printf("esi 0x%8x %8u\n", ref_r->esi);
+  printf("edi 0x%8x %8u\n", ref_r->edi);
+  printf("############################################")
+//rtlreg_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+  */
   return !equal;
 }
 
