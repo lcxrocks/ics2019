151e1a69ae377eba1a4cb324dca992f1e6a8616f
tracer-ics2019 <tracer@njuics.org>
2019-10-24 22:59:55 +0800

    >  compile 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  22:59:55 up 3 days, 11:23,  7 users,  load average: 0.47, 0.33, 0.28 dd92932bac992ac33ed7607237785ce2689831b0

--- nemu/src/isa/x86/exec/logic.c
+++ nemu/src/isa/x86/exec/logic.c
@@ -13,6 +13,8 @@ make_EHelper(test) {
 
 make_EHelper(and) {
   rtl_and(&s0,&id_dest->val, &id_src->val);
+  printf("&&&&&&&&&&&&&&&&&&&&&&&&& id_dest->val: %8x", id_dest->val);
+  printf("&&&&&&&&&&&&&&&&&&&&&&&&& id_src->val: %8x", id_src->val);
   operand_write(id_dest,&s0);
   print_asm_template2(and);
 }
