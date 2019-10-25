9f5e57e7ed0a1db7704b938be1b083381d25e56e
tracer-ics2019 <tracer@njuics.org>
2019-10-25 21:06:50 +0800

    >  compile 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:06:50 up 3 days, 16:47,  7 users,  load average: 0.23, 0.39, 0.40 4843f74c452a12c91231db0a8da41ba47a966bb

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -67,7 +67,7 @@ static OpcodeEntry opcode_table [512] = { //stores OpcodeEntry, which includes d
   /* 0x30 */	EMPTY, IDEX(G2E,xor), EMPTY, EMPTY,
   /* 0x34 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x38 */	EMPTY, EMPTY, EMPTY, IDEXW(G2E,cmp,1),
-  /* 0x3c */	EMPTY, EMPTY, EMPTY, EMPTY,
+  /* 0x3c */	EMPTY, IDEX(I2a,cmp), EMPTY, EMPTY,
   /* 0x40 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x44 */	EMPTY, EMPTY, EMPTY, IDEX(r,inc),
   /* 0x48 */	EMPTY, EMPTY, EMPTY, EMPTY,
