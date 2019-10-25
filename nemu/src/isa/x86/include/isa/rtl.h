b0a9c51924045e620e57c7d1273bfd881efcf012
tracer-ics2019 <tracer@njuics.org>
2019-10-25 21:00:10 +0800

    >  compile 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:00:10 up 3 days, 16:40,  7 users,  load average: 0.52, 0.49, 0.41 f1fb84e72ae81810a84b8c0a98ecfec87e64a69b

--- nemu/src/isa/x86/include/isa/rtl.h
+++ nemu/src/isa/x86/include/isa/rtl.h
@@ -101,17 +101,17 @@ make_rtl_setget_eflags(SF)
 static inline void rtl_update_ZF(const rtlreg_t* result, int width) {
   // eflags.ZF <- is_zero(result[width * 8 - 1 .. 0])
   t0=(4-width)*8;//remove the redundant bits out of our sight
-  printf("cpu.ZF=%8x",cpu.ZF);
+  //printf("cpu.ZF=%8x",cpu.ZF);
   cpu.ZF=((*result & (0xFFFFFFFF)>>t0))== 0;
-  printf("cpu.ZF=%8x",cpu.ZF);
+  //printf("cpu.ZF=%8x",cpu.ZF);
 }
 
 static inline void rtl_update_SF(const rtlreg_t* result, int width) {
   // eflags.SF <- is_sign(result[width * 8 - 1 .. 0])
   t0=width*8-1;//lower-bits
-  printf("cpu.SF=%8x",cpu.SF);
+  //printf("cpu.SF=%8x",cpu.SF);
   cpu.SF=((*result >> t0) & 1);
-  printf("cpu.SF=%8x",cpu.SF);
+  //printf("cpu.SF=%8x",cpu.SF);
 }
 
 static inline void rtl_update_ZFSF(const rtlreg_t* result, int width) {
