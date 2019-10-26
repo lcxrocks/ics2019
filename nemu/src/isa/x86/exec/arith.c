a1534b0cbd64490e889f552d7b664bd8be7f1895
tracer-ics2019 <tracer@njuics.org>
2019-10-26 10:59:28 +0800

    >  compile 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  10:59:28 up 3 days, 23:08,  7 users,  load average: 0.14, 0.16, 0.19 b5c13cda2f84d56fd164688e6821cce47f97ad29

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -44,15 +44,15 @@ make_EHelper(cmp) {
   printf("s0::::::::::::::::::::%8x\n",s0);
   //update ZFSF
   rtl_update_ZFSF(&s0, id_dest->width);
-  //rtl_setrelop()
+  
   // update CF
-  rtl_is_add_carry(&s1,&s0, &id_src->val); //dest, res, src1
-  rtl_is_add_carry(&s0, &s0, &id_dest->val);
+  rtl_is_sub_carry(&s1,&s0, &id_src->val); //dest, res, src1
+  rtl_is_sub_carry(&s0, &s0, &id_dest->val);
   rtl_or(&s0, &s0, &s1);
   rtl_set_CF(&s0);
 
   // update OF
-  rtl_is_add_overflow(&s0, &s0, &id_dest->val, &id_src->val, id_dest->width);
+  rtl_is_sub_overflow(&s0, &s0, &id_dest->val, &id_src->val, id_dest->width);
   rtl_set_OF(&s0);
 
   print_asm_template2(cmp);
