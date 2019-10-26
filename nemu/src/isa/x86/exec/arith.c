f91bbe18464573e3e24c7ff357b23ffba3aae9f1
tracer-ics2019 <tracer@njuics.org>
2019-10-26 12:00:33 +0800

    >  compile 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  12:00:33 up 4 days, 9 min,  7 users,  load average: 0.49, 0.17, 0.13 bff2375641086572e82ab5e74937c0e010936fc8

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -46,13 +46,13 @@ make_EHelper(cmp) {
   rtl_update_ZFSF(&s0, id_dest->width);
   
   // update CF
-  rtl_is_add_carry(&s1,&s0, &id_src->val); //dest, res, src1
-  rtl_is_add_carry(&s0, &s0, &id_dest->val);
+  rtl_is_sub_carry(&s1,&s0, &id_src->val); //dest, res, src1
+  rtl_is_sub_carry(&s0, &s0, &id_dest->val);
   rtl_or(&s0, &s0, &s1);
   rtl_set_CF(&s0);
   
   // update OF
-  rtl_is_sub_overflow(&s0, &s0, &id_dest->val, &id_src->val, id_dest->width);
+  rtl_is_add_overflow(&s0, &s0, &id_dest->val, &id_src->val, id_dest->width);
   rtl_set_OF(&s0);
 
   print_asm_template2(cmp);
