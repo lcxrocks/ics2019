bb911ec1d237b44eeb26ca0e6553388a2bb91813
tracer-ics2019 <tracer@njuics.org>
2019-10-26 21:02:53 +0800

    >  compile 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:02:53 up 4 days,  7:01,  7 users,  load average: 0.13, 0.21, 0.18 2d95f995b812180ce1fd8b3d63fce799260b57a1

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -125,7 +125,7 @@ make_EHelper(sbb) {
   // s1 = s0 - CF
   rtl_get_CF(&s1);
   rtl_sub(&s1, &s0, &s1);
-  uint32_t res=s1;
+  //uint32_t res=s1;
   operand_write(id_dest, &s1);
 
   if (id_dest->width != 4) {
@@ -141,7 +141,7 @@ make_EHelper(sbb) {
   rtl_set_CF(&s0);//cpu.CF=&s0
   
   // update OF
-  rtl_is_sub_overflow(&s0, &res, &id_dest->val, &id_src->val, id_dest->width);//dest, res, src1, src2, width
+  rtl_is_sub_overflow(&s0, &s1, &id_dest->val, &id_src->val, id_dest->width);//dest, res, src1, src2, width
   rtl_set_OF(&s0);//cpu.OF=&s0
 
   print_asm_template2(sbb);
