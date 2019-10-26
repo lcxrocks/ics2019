677bc7f662d8307353fe5affb76d6a75ebf700b9
tracer-ics2019 <tracer@njuics.org>
2019-10-26 22:41:20 +0800

    >  compile 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  22:41:20 up 4 days,  8:00,  7 users,  load average: 0.47, 0.19, 0.09 93780e07453e1d8bedc701f8d8996fb7a30307ad

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -39,8 +39,8 @@ make_EHelper(sub) {
 
 make_EHelper(cmp) {
   rtl_sub(&s0,&id_dest->val, &id_src->val);
-  //printf("$$$$$$$$$$$$$$$$$$$$$$$ id_dest->val: %8x, id_src->val: %8x\n",id_dest->val, id_src->val);
-  //printf("s0::::::::::::::::::::%8x\n",s0);
+  printf("$$$$$$$$$$$$$$$$$$$$$$$ id_dest->val: %8x, id_src->val: %8x\n",id_dest->val, id_src->val);
+  printf("s0::::::::::::::::::::%8x\n",s0);
   //update ZFSF
   rtl_update_ZFSF(&s0, id_dest->width);
   uint32_t res=s0;
