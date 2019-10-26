e8b8a6ddbe152b195e53b101fd2784d167eb2a68
tracer-ics2019 <tracer@njuics.org>
2019-10-26 12:00:20 +0800

    >  compile 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  12:00:20 up 4 days, 9 min,  7 users,  load average: 0.34, 0.13, 0.11 5c48168e5e456faaf82b763109ba3ab66105b09

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -52,7 +52,7 @@ make_EHelper(cmp) {
   rtl_set_CF(&s0);
   
   // update OF
-  rtl_is_add_overflow(&s0, &s0, &id_dest->val, &id_src->val, id_dest->width);
+  rtl_is_sub_overflow(&s0, &s0, &id_dest->val, &id_src->val, id_dest->width);
   rtl_set_OF(&s0);
 
   print_asm_template2(cmp);
