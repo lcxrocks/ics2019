8dbd86020e0c6c0f2461f9047258b9b067d1af0f
tracer-ics2019 <tracer@njuics.org>
2019-10-26 11:14:39 +0800

    >  compile 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  11:14:39 up 3 days, 23:23,  7 users,  load average: 0.53, 0.31, 0.22 b0b68163efe85cf13ae5c4cb1f3ba923ec129ab

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -50,7 +50,7 @@ make_EHelper(cmp) {
   rtl_is_sub_carry(&s0, &s0, &id_dest->val);
   rtl_or(&s0, &s0, &s1);
   rtl_set_CF(&s0);
-
+  printf("#########CF: %8x\n", cpu.CF);
   // update OF
   rtl_is_sub_overflow(&s0, &s0, &id_dest->val, &id_src->val, id_dest->width);
   rtl_set_OF(&s0);
@@ -130,7 +130,7 @@ make_EHelper(sbb) {
   rtl_is_sub_carry(&s0, &s0, &id_dest->val);
   rtl_or(&s0, &s0, &s1);//dest, a,b ;
   rtl_set_CF(&s0);//cpu.CF=&s0
-  printf("#########CF: %8x\n", cpu.CF);
+  
   // update OF
   rtl_is_sub_overflow(&s0, &s1, &id_dest->val, &id_src->val, id_dest->width);//dest, res, src1, src2, width
   rtl_set_OF(&s0);//cpu.OF=&s0
