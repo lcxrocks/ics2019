a80a3905a8723ce8759ad833881193f1c8565df9
tracer-ics2019 <tracer@njuics.org>
2019-10-30 21:34:04 +0800

    >  run 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:34:04 up 4 days, 21:48,  7 users,  load average: 0.36, 0.31, 0.18 80b097033f4171635874dc566a164c692c1913af

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -8,11 +8,36 @@ int printf(const char *fmt, ...) {
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  return 0;
+  int cnt = 0;
+  for (; *fmt; fmt++)
+  {
+    if( *fmt != '%' ){
+      *out++ = *fmt;
+      continue;
+    }
+    cnt++;
+    fmt++; //skip '%'
+    switch(*fmt){
+      case 's': strcat(out,va_arg(ap,char *));
+                continue;
+
+      case 'd': *out++ = va_arg(ap,int);
+                continue;
+    }
+  }
+  
+  return cnt;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  return 0;
+  int ans;
+  va_list ap;
+
+  va_start(ap,fmt);
+  ans = vsprintf(out,fmt,ap);
+  va_end(ap);
+
+  return ans;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
