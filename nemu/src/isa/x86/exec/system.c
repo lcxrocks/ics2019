a6a9be7ee1b6d6b50cc75bfc275b0d8e711d6a26
tracer-ics2019 <tracer@njuics.org>
2019-11-01 21:51:33 +0800

    >  compile 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:51:33 up 5 days, 22:01, 11 users,  load average: 0.36, 0.40, 0.44 2a4bf742711627e8bad7af9fd446bf3ddb606738

--- nemu/src/isa/x86/exec/system.c
+++ nemu/src/isa/x86/exec/system.c
@@ -66,14 +66,14 @@ make_EHelper(in) {
 }
 
 make_EHelper(out) {
-  rtl_host_lm(&s0, &id_dest->val, id_dest->width);
-  rtl_host_lm(&s1, &id_src->val,id_src->width);
-  printf("s0->width: %8x, s1->width: %8x hah\n ", id_dest->width,id_src->width);
+  //rtl_host_lm(&s0, &id_dest->val, id_dest->width);
+  //rtl_host_lm(&s1, &id_src->val,id_src->width);
+  //printf("s0->width: %8x, s1->width: %8x hah\n ", id_dest->width,id_src->width);
 
   switch(id_src->width){
     case 1: pio_write_b(id_dest->val,id_src->val); break;
-    case 2: pio_write_w(s0,s1); break;
-    case 4: pio_write_l(s0,s1); break;
+    ///case 2: pio_write_w(s0,s1); break;
+    //case 4: pio_write_l(s0,s1); break;
     default: printf("func Ehelper_out() down at %d\n", __LINE__); assert(0); break;
   }
 
