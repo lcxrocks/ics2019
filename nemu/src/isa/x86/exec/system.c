0803101c532c7beceba366831849859669fb1254
tracer-ics2019 <tracer@njuics.org>
2019-11-01 21:32:45 +0800

    >  compile 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:32:45 up 5 days, 21:42, 11 users,  load average: 1.06, 0.92, 0.57 cddb5716d1625e9f9bda1145451349f65487e15

--- nemu/src/isa/x86/exec/system.c
+++ nemu/src/isa/x86/exec/system.c
@@ -42,8 +42,26 @@ void pio_write_w(ioaddr_t, uint32_t);
 void pio_write_b(ioaddr_t, uint32_t);
 
 make_EHelper(in) {
-  TODO();
+  //TODO();
+  rtl_host_lm(&s0,&id_src->val,id_src->width);
+  switch (id_dest->width)
+  {
+  case 1:
+    s1 = pio_read_b(s0);
+    break;
+  case 2:
+    s1 = pio_read_w(s0);
+    break;
+  case 4:
+    s1 = pio_read_l(s0);
+    break;
 
+  default:
+    printf("func Ehelper_in() down at %d\n", __LINE__); 
+    assert(0);
+    break;
+  }
+  operand_write(id_dest, &s1);
   print_asm_template2(in);
 }
 
