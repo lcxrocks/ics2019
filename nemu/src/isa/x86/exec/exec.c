715fb8bec05f57d83714e37574e35cd460d8f906
tracer-ics2019 <tracer@njuics.org>
2019-11-02 23:38:39 +0800

    >  compile 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  23:38:39 up  8:14,  8 users,  load average: 0.81, 0.60, 0.42 1f5a1af1dc4c7fcbd8d8f9f4a651108fd8b75b

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -22,7 +22,7 @@ static make_EHelper(name) { \
 /* 0x80, 0x81, 0x83 */
 make_group(gp1,
     EX(add), EX(or), EMPTY, EX(sbb),
-    EX(and), EX(sub), EMPTY, EX(cmp)) //dont need decode_ID
+    EX(and), EX(sub), EX(xor), EX(cmp)) //dont need decode_ID
 
 /* 0xc0, 0xc1, 0xd0, 0xd1, 0xd2, 0xd3 */
 make_group(gp2,
@@ -52,7 +52,7 @@ make_group(gp7,
 /* TODO: Add more instructions!!! */
 
 static OpcodeEntry opcode_table [512] = { //stores OpcodeEntry, which includes decode func and exec func and width of each opcode.
-  /* 0x00 */	IDEXW(G2E,add,1), IDEX(G2E,add),IDEXW(E2G,add,1) , IDEX(E2G,add),
+  /* 0x00 */	IDEXW(G2E,add,1), IDEX(G2E,add),IDEXW(E2G,add,1),IDEX(E2G,add),
   /* 0x04 */	EMPTY, EMPTY, EMPTY, EMPTY,
   /* 0x08 */	IDEXW(G2E,or,1), IDEX(G2E,or), IDEXW(E2G,or,1), IDEX(E2G,or),
   /* 0x0c */	EMPTY, EMPTY, EMPTY, EX(2byte_esc),
