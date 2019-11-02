2a90a542374c5b33983a2d700023c9947a112902
tracer-ics2019 <tracer@njuics.org>
2019-11-02 20:13:49 +0800

    >  compile 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  20:13:49 up  5:30,  5 users,  load average: 0.56, 0.37, 0.51 1b2e840110febcae84af0e727f947d2bd0b5ebc1

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -5,9 +5,10 @@
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
+      uint32_t Video = inl(SCREEN_ADDR);
       _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
-      info->width = 0;
-      info->height = 0;
+      info->width = Video;
+      info->height = Video;
       return sizeof(_DEV_VIDEO_INFO_t);
     }
   }
