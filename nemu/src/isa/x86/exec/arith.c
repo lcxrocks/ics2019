d5c597eadfa8aa1e2d87dc1f5500513468ae2b59
tracer-ics2019 <tracer@njuics.org>
2019-11-02 19:11:47 +0800

    >  compile 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  19:11:47 up  4:27,  5 users,  load average: 0.81, 0.66, 0.50 4344b6c4f7df6917236f4e9b71e00768c9bbc828

--- nemu/src/isa/x86/exec/arith.c
+++ nemu/src/isa/x86/exec/arith.c
@@ -88,17 +88,16 @@ make_EHelper(dec) {
 }
 
 make_EHelper(neg) {
-  printf("fuck id_src: %8x\n",id_dest->val);
-  if(id_src->val==0) s0 = 0;   
+
+  if(id_dest->val==0) s0 = 0;   
   else s0 = 1;
   rtl_set_CF(&s0);
   /* neg = ~ + 1 */
   s0 = 1;
-  /*printf("id_src: %8x\n",id_src->val);
-  rtl_not(&id_dest->val,&id_src->val);
+  rtl_not(&id_dest->val,&id_dest->val);
   printf("id_dest: %8x\n",id_dest->val);
   rtl_add(&id_dest->val,&id_dest->val,&s0);
-  operand_write(id_dest,&id_dest->val);*/
+  operand_write(id_dest,&id_dest->val);
   print_asm_template1(neg);
 }
 
