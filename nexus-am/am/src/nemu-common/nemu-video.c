96e74d5bc2a76a0175dbc1214e3e64cbe8b68232
tracer-ics2019 <tracer@njuics.org>
2019-11-02 21:08:39 +0800

    >  compile 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:08:39 up  6:24,  5 users,  load average: 0.47, 0.27, 0.25 317da955298fcba1d321bcc212687c0b5b396f9b

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -7,8 +7,8 @@ size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
     case _DEVREG_VIDEO_INFO: {
       uint32_t Video = inl(SCREEN_ADDR);
       _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
-      info->width = Video;
-      info->height = Video;
+      info->width = Video >> 16;
+      info->height = Video & 0xffff; //attributed__(packed)
       return sizeof(_DEV_VIDEO_INFO_t);
     }
   }
@@ -19,7 +19,12 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
-
+      /*  by lcx
+      int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+      uint32_t *pixels = ctl->pixels;
+      int W = screen_width(), H = screen_height();
+      int cp_bytes = sizeof(uint32_t) * (w < W-x ? w : W-x);
+      */
       if (ctl->sync) {
         outl(SYNC_ADDR, 0);
       }
