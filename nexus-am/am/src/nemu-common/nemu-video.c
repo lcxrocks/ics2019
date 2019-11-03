468110b092b8f6779abbfaa2976c37da07c498cf
tracer-ics2019 <tracer@njuics.org>
2019-11-03 19:16:20 +0800

    >  run 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  19:16:20 up 16:57,  8 users,  load average: 0.28, 0.28, 0.22 37eac80264362eac6cd45bc5b56e56645e01f246

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -26,15 +26,17 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
       /*  by lcx  */
-      /*int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+      int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
       uint32_t *pixels = ctl->pixels;
       //int W = screen_width(), H = screen_height();
       int cp_bytes = sizeof(uint32_t) * (w < W-x ? w : W-x);
       
       for (int j = 0; j < h && y + j < H; j ++) {
+        printf("pixels: %d\n",pixels);
         memcpy(&fb[(y + j) * W + x], pixels, cp_bytes);
+        printf("fb[j]: %d\n",fb[j]);
         pixels += w;
-      }*/
+      }
       /* copied from native  */
       if (ctl->sync) {
         outl(SYNC_ADDR, 0);
