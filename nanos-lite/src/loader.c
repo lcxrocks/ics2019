92aadc27b5c8c76678edd730282696e1d503005b
tracer-ics2019 <tracer@njuics.org>
2019-11-27 16:11:30 +0800

    >  run 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  16:11:30 up 1 day,  1:04,  4 users,  load average: 0.13, 0.11, 0.09 8ec8d5ce67b86927f4047d4987cedad0ff19f8d3

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -16,15 +16,14 @@ size_t ramdisk_write(const void *buf, size_t offset, size_t len) ;
 size_t get_ramdisk_size() ;
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  //TODO();
-  ramdisk_read((void *)0x3000000,0,get_ramdisk_size());
+  TODO();
+  //ramdisk_read((void *)0x3000000,0,get_ramdisk_size());
   //ramdisk_write((void *)0x3000000,0,get_ramdisk_size());
-  return ((uintptr_t)0x3000000);
+  //return ((uintptr_t)0x3000000);
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
-  printf("entry: %x\n",entry);
   Log("Jump to entry = %x", entry);
   ((void(*)())entry) ();
 }
