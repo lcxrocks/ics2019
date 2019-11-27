fb1f877ab0236b3f68168f4e214e388cb8b74e4b
tracer-ics2019 <tracer@njuics.org>
2019-11-27 09:05:50 +0800

    >  compile 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  09:05:50 up 20:23,  4 users,  load average: 0.49, 0.23, 0.10 d399a2aba371b5be70838a8577ea0ee51f45a64f

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -24,6 +24,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
+  printf("entry: %x\n",entry);
   Log("Jump to entry = %x", entry);
   ((void(*)())entry) ();
 }
