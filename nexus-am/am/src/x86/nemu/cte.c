4b30c894961af58257b068fc6c87d241705add32
tracer-ics2019 <tracer@njuics.org>
2019-11-28 21:58:38 +0800

    >  run 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:58:38 up 1 day, 21:11,  4 users,  load average: 0.53, 0.26, 0.20 141868b27212b831ef127b9409231b919a0a928

--- nexus-am/am/src/x86/nemu/cte.c
+++ nexus-am/am/src/x86/nemu/cte.c
@@ -24,7 +24,7 @@ _Context* __am_irq_handle(_Context *c) {
     _Event ev = {0};
     switch (c->irq) {
       case 0x81 : ev.event = _EVENT_YIELD; break;
-      case 0x80 : ev.event = _EVENT_SYSCALL; break;
+      case 0x80 : printf("hhahahahahha\n"); ev.event = _EVENT_SYSCALL; break;
       default: ev.event = _EVENT_ERROR; break;
     }
 
