8f15eeb3adec5cf9871d9690b02137a173aeaf64
tracer-ics2019 <tracer@njuics.org>
2019-12-02 23:56:12 +0800

    >  run 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  23:56:12 up 3 days,  9:53,  4 users,  load average: 0.53, 0.34, 0.18 44e5d39195a9d20dfeaea206ffe770143cd0d13c

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,34 +1,37 @@
 #include "common.h"
 #include "syscall.h"
+int sys_write(int fd, void *buf, size_t count);
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1; //sys-call type
-  a[1] = c->GPR2; //arg1: fd
-  a[2] = c->GPR3; //arg2: buf
-  a[3] = c->GPR4; //arg3: count
+  a[1] = c->GPR2; //arg1: 
+  a[2] = c->GPR3; //arg2: 
+  a[3] = c->GPR4; //arg3: 
   //
   //printf("--------------a[0]: %x--------------\n",a[0]);
   switch (a[0]) {
     case SYS_exit: _halt(c->GPRx); break;
     case SYS_yield: _yield(); c->GPRx = 0; break;
-    case SYS_write: if(a[1]==1||a[1]==2) //stdout/stderr
-                    { 
-                      //printf("sys_write !!!!\n");
-                      int cnt = 0;
-                      for (int i = 0; i < a[3]; i++)
-                      {
-                        _putc(((const char*)a[2])[i]);
-                        cnt++;
-                      }
-                      c->GPRx = cnt;
-                    }            
-                    else
-                    {
-                      c->GPRx = -1;
-                    }
-                    break;
+    case SYS_write: c->GPRx = sys_write((int)a[1],(void *)a[2],(size_t)a[3]);
+    //case SYS_brk: a[1]
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
   return NULL;
+}
+
+int sys_write(int fd, void *buf, size_t count)
+{
+  if(fd==1||fd==2) //stdout/stderr
+  { 
+    int cnt = 0;
+    for (int i = 0; i < count; i++)
+    {
+       _putc(((const char*)buf)[i]);
+       cnt++;
+    }
+    return cnt;
+  }            
+  else
+  return -1;
 }
