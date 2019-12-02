271ae553ac3962846b30f2d37f0c6699a1c8ae72
tracer-ics2019 <tracer@njuics.org>
2019-12-02 19:57:58 +0800

    >  run 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  19:57:58 up 3 days,  5:57,  4 users,  load average: 0.65, 0.39, 0.22 4e5f7a432d976a067abf87e1623b90129809c666

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,7 +2,18 @@
 _Context* do_syscall(_Context *c);
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
-    case _EVENT_YIELD: Log("haha,hit _EVENT_YIELD\n"); break;
+    case _EVENT_YIELD: Log("haha,hit _EVENT_YIELD\n"); 
+      printf("-----context \% edi: 0x%x----\n",c->edi);
+  printf("-----context \% esi: 0x%x----\n",c->esi);
+  printf("-----context \% ebp: 0x%x----\n",c->ebp);
+  printf("-----context \% esp: 0x%x----\n",c->esp);
+  printf("-----context \% ebx: 0x%x----\n",c->ebx);
+  printf("-----context \% edx: 0x%x----\n",c->edx);
+  printf("-----context \% ecx: 0x%x----\n",c->ecx);
+  printf("-----context \% eax: 0x%x----\n",c->eax);
+  printf("-----context c.irq : 0x%x----\n",c->irq);
+    
+    break;
     case _EVENT_SYSCALL: printf("syscall!\n"); do_syscall(c); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
