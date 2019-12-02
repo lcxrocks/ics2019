3ca79a6d50f0a51f5babbf361e9f56ad50653e14
tracer-ics2019 <tracer@njuics.org>
2019-12-02 21:42:23 +0800

    >  run 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:42:23 up 3 days,  7:39,  4 users,  load average: 0.29, 0.11, 0.08 d823ce485c6493fc16fee334d16961896b93e37

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -17,10 +17,44 @@ size_t get_ramdisk_size() ;
 
 static uintptr_t loader(PCB *pcb, const char *filename) {
   //TODO();
-  ramdisk_read((void *)0x3001000,0x1000,0x3a43);
-  ramdisk_read((void *)0x3005000,0x5000,0x1918);
-  ramdisk_read((void *)0x3008000,0x7000,0x8d8);
-  return ((uintptr_t)0x3000000);
+  // ramdisk_read((void *)0x3001000,0x1000,0x3a43);
+  // ramdisk_read((void *)0x3005000,0x5000,0x1918);
+  // ramdisk_read((void *)0x3008000,0x7000,0x8d8);
+  // return ((uintptr_t)0x3000000);
+  Elf_Ehdr ehdr;
+  ramdisk_read(&ehdr, 0, sizeof(ehdr));
+//   typedef struct
+// {
+//   Elf32_Word	p_type;			/* Segment type */
+//   Elf32_Off	p_offset;		/* Segment file offset */
+//   Elf32_Addr	p_vaddr;		/* Segment virtual address */
+//   Elf32_Addr	p_paddr;		/* Segment physical address */
+//   Elf32_Word	p_filesz;		/* Segment size in file */
+//   Elf32_Word	p_memsz;		/* Segment size in memory */
+//   Elf32_Word	p_flags;		/* Segment flags */
+//   Elf32_Word	p_align;		/* Segment alignment */
+// } Elf32_Phdr;
+
+  Elf_Phdr phdr[ehdr.e_phnum]; //segement view
+  ramdisk_read(&phdr, ehdr.e_phoff, ehdr.e_phentsize * ehdr.e_phnum);
+
+  for (uint16_t i = 0; i < ehdr.e_phnum; i++)
+  {
+    if (phdr[i].p_type == PT_LOAD)
+    {
+      size_t content[phdr[i].p_filesz];
+      ramdisk_read(content, phdr[i].p_offset, phdr[i].p_filesz);
+      uint32_t *ptr1 = (uint32_t *)phdr[i].p_vaddr;
+      memcpy(ptr1, content, phdr[i].p_filesz);
+
+      if (phdr[i].p_memsz > phdr[i].p_filesz) //.bss
+      {
+        char *ptr2 = (char *)(phdr[i].p_vaddr + phdr[i].p_filesz);
+        memset(ptr2, 0, phdr[i].p_memsz - phdr[i].p_filesz);
+      }
+    }
+  }
+return ehdr.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
