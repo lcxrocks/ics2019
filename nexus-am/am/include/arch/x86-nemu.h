8331548589fdd061249d14e15a5d87d0461f2ed2
tracer-ics2019 <tracer@njuics.org>
2019-12-02 09:46:21 +0800

    >  run 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  09:46:21 up 3 days,  3:10,  4 users,  load average: 0.06, 0.21, 0.25 47a169691c58bb65ad20205bd2e58c4caf5cf9dd

--- nexus-am/am/include/arch/x86-nemu.h
+++ nexus-am/am/include/arch/x86-nemu.h
@@ -5,21 +5,10 @@ struct _Context {
   struct _AddressSpace *as;
   uintptr_t edi,esi,ebp,esp,ebx,edx,ecx,eax; //gprs
   int irq;
-  //uintptr_t err;
   uintptr_t pc;
   uintptr_t cs;
   uintptr_t eflag;
-  //?
-     /*
-   Temp := (SP);
-   Push(AX);
-   Push(CX);
-   Push(DX);
-   Push(BX);
-   Push(Temp);
-   Push(BP);
-   Push(SI);
-   Push(DI);*/
+  
 };
 
 #define GPR1 eax
