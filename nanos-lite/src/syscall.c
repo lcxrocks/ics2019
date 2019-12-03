c9a6c3c762184b747bf8a779474eb7069d84dfb9
tracer-ics2019 <tracer@njuics.org>
2019-12-03 22:35:28 +0800

    >  run 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  22:35:28 up 3 days, 20:09,  4 users,  load average: 0.72, 0.42, 0.23 247803b1aa988ebaef088a98b38cf4a84f9ddb4e

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -14,7 +14,7 @@ _Context* do_syscall(_Context *c) {
   switch (a[0]) {
     case SYS_exit: _halt(c->GPRx); break;
     case SYS_yield: _yield(); c->GPRx = 0; break;
-    //case SYS_open: c->GPRx = fs_open((char *)a[1]); break;
+    case SYS_open: c->GPRx = fs_open((char *)a[1]); break;
     case SYS_read: c->GPRx = fs_read((int)a[1],(void *)a[2],(size_t)a[3]); break;
     case SYS_write: c->GPRx = sys_write((int)a[1],(void *)a[2],(size_t)a[3]); break;
     case SYS_lseek: c->GPRx = fs_lseek((int)a[1],(size_t)a[2],(int)a[3]); break;
