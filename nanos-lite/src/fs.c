6c8843b62bf1d11aa6264e83c3f1068184caca36
tracer-ics2019 <tracer@njuics.org>
2019-12-05 00:32:18 +0800

    >  run 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  00:32:18 up 4 days, 12:42,  4 users,  load average: 0.08, 0.15, 0.17 ff108466e9cb82cfa1e27d0676170d40ebb17801

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -32,7 +32,8 @@ static Finfo file_table[] __attribute__((used)) = {
   {"stdin", 0, 0, invalid_read, invalid_write}, //占位
   {"stdout", 0, 0, invalid_read, serial_write},
   {"stderr", 0, 0, invalid_read, serial_write},
-  {"/dev/events" ,0,0, events_read, invalid_write}, //FUCK
+  {"/dev/fb",0, 0, invalid_read,serail_write},
+  {"/dev/events" ,0,0, events_read, invalid_write}, 
   
 #include "files.h"
 };
