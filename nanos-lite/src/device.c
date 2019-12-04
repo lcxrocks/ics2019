57ca89b2836ac0a5e47f6e036028471e76c4a5d9
181240035-Liu Chunxu <181240035@smai.nju.edu.cn>
2019-12-05 01:10:16 +0800

    #19 Added fb_sync

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -63,7 +63,8 @@ size_t fb_write(const void *buf, size_t offset, size_t len) {
 }
 
 size_t fbsync_write(const void *buf, size_t offset, size_t len) {
-  return 0;
+  draw_sync();
+  return len;
 }
 
 void init_device() {
