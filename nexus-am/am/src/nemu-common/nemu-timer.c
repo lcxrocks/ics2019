8575553962ec59b61c76a6bd1a216f60db3ea729
tracer-ics2019 <tracer@njuics.org>
2019-12-04 23:44:00 +0800

    >  run 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  23:44:00 up 4 days, 11:54,  4 users,  load average: 0.34, 0.22, 0.23 9051123a8fc358049fb80f7f340270e86b3b7823

--- nexus-am/am/src/nemu-common/nemu-timer.c
+++ nexus-am/am/src/nemu-common/nemu-timer.c
@@ -11,7 +11,7 @@ size_t __am_timer_read(uintptr_t reg, void *buf, size_t size) {
       uint32_t cur_time = inl(RTC_ADDR);
       printf("cur_time: %d\n",cur_time);
       printf("start_time: %d\n",starttime);
-      printf("ahahhahaha%d\n",1234557);
+      printf("ahahhahaha%d\n",-1234557);
       /* current time */
       _DEV_TIMER_UPTIME_t *uptime = (_DEV_TIMER_UPTIME_t *)buf;
       uptime->hi = 0;
