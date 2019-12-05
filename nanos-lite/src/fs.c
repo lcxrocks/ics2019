bf6092a4526390e6877fde08e7e8db37725c3aaf
tracer-ics2019 <tracer@njuics.org>
2019-12-06 02:09:21 +0800

    >  run 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  02:09:21 up 5 days,  7:36,  4 users,  load average: 0.71, 0.53, 0.42 d5e178bb62b25937d622b6ffe4723f06ee3a24b

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -84,8 +84,8 @@ size_t fs_read(int fd, void *buf, size_t len)
       len = size - open_offset;
   size_t ret=0;
   Log("fs_read: fd: %d, len:%d\n",fd, len);
-  if(file_table[fd].read){
-    Log("ret: %d\n",ret);
+  if(fd<10&&file_table[fd].read){
+    //Log("ret: %d\n",ret);
     ret = file_table[fd].read(buf, read_start, len);
     //Log("ret: %d\n",ret);
   }
