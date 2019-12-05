1802d091691947c8487865f4ce0e25573d5888b3
tracer-ics2019 <tracer@njuics.org>
2019-12-06 01:07:24 +0800

    >  run 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  01:07:24 up 5 days,  6:34,  4 users,  load average: 0.62, 0.38, 0.34 5a4cf77dcc6037785b3461cbe055a03e1fafe3c

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -42,7 +42,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) { //
       uint32_t *p_start = (uint32_t *)phdr[i].p_vaddr;
       //fs_write(fd, p_start, phdr[i].p_filesz);
       memcpy(p_start, content, phdr[i].p_filesz);
-
+      Log("Finished load phdr[%d], p_start:%x, content_length:%d, p_filesz:%d\n",i,p_start, sizeof(content), phdr[i].p_filesz);
       if (phdr[i].p_memsz > phdr[i].p_filesz) //.bss
       {
         char *bss_start = (char *)(phdr[i].p_vaddr + phdr[i].p_filesz);
