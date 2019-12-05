ff861792e3ffa842ffd88bdfac4977fe89c11f5c
tracer-ics2019 <tracer@njuics.org>
2019-12-06 01:55:55 +0800

    >  run 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  01:55:55 up 5 days,  7:22,  4 users,  load average: 0.64, 0.41, 0.36 20fff7f1cab2a5c955b53b58a05ae75f591a7235

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -92,26 +92,6 @@ size_t fs_read(int fd, void *buf, size_t len)
   return ret;
 }
 
-// size_t fs_read(int fd, void *buf, size_t len){
-//   if (fd == 4){
-//     return file_table[fd].read(buf, 0, len);
-//   }
-//   size_t ptr_end = file_table[fd].open_offset + len;
-//   if (ptr_end > file_table[fd].size){
-//     len = file_table[fd].size - file_table[fd].open_offset;
-//     ptr_end = file_table[fd].size;
-//   }
-//   size_t ptr_head = file_table[fd].disk_offset + file_table[fd].open_offset;
-//   if (file_table[fd].read != NULL){
-//     file_table[fd].read(buf, ptr_head, len);
-//   }else{
-//     ramdisk_read(buf, ptr_head, len);
-//   }
-//   file_table[fd].open_offset = ptr_end;
-//   return len;
-// };
-
-
 size_t fs_lseek(int fd, size_t offset, int whence)
 {
   switch (whence)
