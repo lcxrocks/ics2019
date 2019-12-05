f57b3e35be86458da373b53decbec5b31b8ede6d
tracer-ics2019 <tracer@njuics.org>
2019-12-06 01:52:33 +0800

    >  run 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  01:52:33 up 5 days,  7:19,  4 users,  load average: 0.51, 0.39, 0.35 5742bc05f99d568b2e925251714d6c6c99862b

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -73,44 +73,45 @@ int fs_close(int fd)
   return 0;
 }
 
-size_t fs_read(int fd, void *buf, size_t len)
-{
-  Log("---fd: %d ---buf:%s; --- len:%d\n",fd, buf,len);
-  int size = file_table[fd].size; 
-  int disk_offset = file_table[fd].disk_offset;
-  int open_offset = file_table[fd].open_offset;
-  size_t read_start = disk_offset + open_offset;
-  size_t ram_read_len = len;
-  int ret;
-  if(open_offset + len > size)
-      ram_read_len = size - open_offset;
-  if(!file_table[fd].read)
-    ret = ramdisk_read(buf, read_start,ram_read_len);
-  else
-    ret = file_table[fd].read(buf, disk_offset+open_offset, len);
-  file_table[fd].open_offset += ret;
-  //printf("ret: %d\n",ret); 
-  return ret;
-}
+// size_t fs_read(int fd, void *buf, size_t len)
+// {
+//   Log("---fd: %d ---buf:%s; --- len:%d\n",fd, buf,len);
+//   int size = file_table[fd].size; 
+//   int disk_offset = file_table[fd].disk_offset;
+//   int open_offset = file_table[fd].open_offset;
+//   size_t read_start = disk_offset + open_offset;
+//   size_t ram_read_len = len;
+  
+//   if(open_offset + len > size)
+//       ram_read_len = size - open_offset;
+//   int ret;
+//   if(!file_table[fd].read)
+//     ret = ramdisk_read(buf, read_start,ram_read_len);
+//   else
+//     ret = file_table[fd].read(buf, read_start, len);
+//   file_table[fd].open_offset += ret;
+//   //printf("ret: %d\n",ret); 
+//   return ret;
+// }
 
-// size_t fs_read(int fd, void *buf, size_t len){
-//   if (fd == 4){
-//     return file_table[fd].read(buf, 0, len);
-//   }
-//   size_t ptr_end = file_table[fd].open_offset + len;
-//   if (ptr_end > file_table[fd].size){
-//     len = file_table[fd].size - file_table[fd].open_offset;
-//     ptr_end = file_table[fd].size;
-//   }
-//   size_t ptr_head = file_table[fd].disk_offset + file_table[fd].open_offset;
-//   if (file_table[fd].read != NULL){
-//     file_table[fd].read(buf, ptr_head, len);
-//   }else{
-//     ramdisk_read(buf, ptr_head, len);
-//   }
-//   file_table[fd].open_offset = ptr_end;
-//   return len;
-// };
+size_t fs_read(int fd, void *buf, size_t len){
+  if (fd == 4){
+    return file_table[fd].read(buf, 0, len);
+  }
+  size_t ptr_end = file_table[fd].open_offset + len;
+  if (ptr_end > file_table[fd].size){
+    len = file_table[fd].size - file_table[fd].open_offset;
+    ptr_end = file_table[fd].size;
+  }
+  size_t ptr_head = file_table[fd].disk_offset + file_table[fd].open_offset;
+  if (file_table[fd].read != NULL){
+    file_table[fd].read(buf, ptr_head, len);
+  }else{
+    ramdisk_read(buf, ptr_head, len);
+  }
+  file_table[fd].open_offset = ptr_end;
+  return len;
+};
 
 
 size_t fs_lseek(int fd, size_t offset, int whence)
@@ -144,7 +145,7 @@ size_t fs_write(int fd, const void *buf, size_t len){
     if(!file_table[fd].write)
       ret = ramdisk_write(buf, write_start,ram_write_len);
     else 
-      ret = file_table[fd].write(buf, disk_offset+open_offset, len);
+      ret = file_table[fd].write(buf, write_start, len);
     file_table[fd].open_offset +=ret;
     return ret; //VFS
 }
