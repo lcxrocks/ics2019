01d9aeb3cdf4a6adfb1576ad8d63d5679e3eb7cb
tracer-ics2019 <tracer@njuics.org>
2019-12-06 00:00:43 +0800

    >  run 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  00:00:43 up 5 days,  5:27,  4 users,  load average: 0.41, 0.38, 0.34 18a9a1bde3b0f90b456b39f7422d2d3452a9583f

--- navy-apps/tests/bmp/main.cpp
+++ navy-apps/tests/bmp/main.cpp
@@ -1,21 +1,21 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <ndl.h>
-#include <cstdio>
+//#include <cstdio>
 
 int main() {
   NDL_Bitmap *bmp = (NDL_Bitmap*)malloc(sizeof(NDL_Bitmap));
   NDL_LoadBitmap(bmp, "/share/pictures/projectn.bmp");
-  printf("NDL_LOADBitMap succes\n");
+  cout<<"NDL_LOADBitMap succes"<<endl;
   assert(bmp->pixels);
   NDL_OpenDisplay(bmp->w, bmp->h);
-  printf("OpenDisplay success\n");
+  cout<<"OpenDisplay success"<<endl;
   NDL_DrawRect(bmp->pixels, 0, 0, bmp->w, bmp->h);
-  printf("DrawRect success\n");
+  cout<<"DrawRect success"<<endl;
   NDL_Render();
-  printf("NDL_Render success\n");
+  cout<<"NDL_Render success"<<endl;
   NDL_CloseDisplay();
-  printf("ND_Close success\n");
+  cout<<"ND_Close success"<<endl;
   while (1);
   return 0;
 }
