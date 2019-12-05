f88e92014ef55bf4330db4bb754243ba11216901
tracer-ics2019 <tracer@njuics.org>
2019-12-06 02:08:07 +0800

    >  run 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  02:08:07 up 5 days,  7:34,  4 users,  load average: 0.56, 0.46, 0.39 31fb54c67409bfd9b1ea18b2721aee33c6d867

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -84,15 +84,15 @@ size_t fs_read(int fd, void *buf, size_t len)
       len = size - open_offset;
   size_t ret=0;
   Log("fs_read: fd: %d, len:%d\n",fd, len);
-  if(!file_table[fd].read){
-    Log("ret: %d\n",ret);
-    ret = ramdisk_read(buf, read_start,len);
-    Log("ret: %d\n",ret);
+  if(file_table[fd].read){
+    //Log("ret: %d\n",ret);
+    ret = file_table[fd].read(buf, read_start, len);
+    //Log("ret: %d\n",ret);
   }
   else{
-    Log("retelse: %d\n",ret);
-    ret = file_table[fd].read(buf, read_start, len);
-    Log("retelse: %d\n",ret);
+    //Log("retelse: %d\n",ret);
+    ret = ramdisk_read(buf, read_start,len);
+    //Log("retelse: %d\n",ret);
   }
   file_table[fd].open_offset += ret;
   //printf("ret: %d\n",ret); 
