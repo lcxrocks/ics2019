aa3e0c835ffa11d6a58a996722ea99d4255bba97
tracer-ics2019 <tracer@njuics.org>
2019-12-06 20:57:07 +0800

    >  run 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  20:57:07 up 5 days, 17:49,  4 users,  load average: 0.22, 0.23, 0.22 1380dfc184e5ae743f40f8b35278c293b3caff1b

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -21,7 +21,7 @@ void hello_fun(void *arg) {
 
 void init_proc() {
   switch_boot_pcb();
-  naive_uload(NULL,"/bin/pal");
+  naive_uload(NULL,"/bin/bmptest");
   Log("Initializing processes...");
 
   // load program here
