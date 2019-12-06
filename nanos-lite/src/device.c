a60bdd863fc9a8a336447b6a1d53faba6860aa13
tracer-ics2019 <tracer@njuics.org>
2019-12-06 23:01:44 +0800

    >  compile 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  23:01:44 up 5 days, 19:44,  4 users,  load average: 0.20, 0.22, 0.19 1cebdc17ee8e865fd53de8857f099affb645acb0

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -39,7 +39,7 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 static char dispinfo[128] __attribute__((used)) = {};
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
-  printf("hahahah is %s\n",dispinfo);
+  //printf("hahahah is %s\n",dispinfo);
   int cnt;
   int size = strlen(dispinfo);
   Log("buf:%s ---offset:%d---len:%d---dispinfosize:%d\n",buf, offset,len,size);
