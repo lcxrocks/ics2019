4b75767e29fe6cfe1c5d346b9176ddd328f7a35c
tracer-ics2019 <tracer@njuics.org>
2019-12-07 01:28:06 +0800

    >  run 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  01:28:06 up 5 days, 22:10,  4 users,  load average: 0.80, 0.54, 0.41 7880b0033791497379a211ac1c3d9b24fd03b37a

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -103,11 +103,13 @@ size_t fs_read(int fd,void *buf,size_t len)
 {
 	if(file_table[fd].read==NULL)
 	{
+    Log("read ==NULL\n");
     if(file_table[fd].open_offset+len>file_table[fd].size)len=file_table[fd].size-file_table[fd].open_offset;
 	  len=ramdisk_read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
 	}
 	else 
 	{
+    Log("read != NULL\n");
     len=file_table[fd].read(buf,file_table[fd].disk_offset+file_table[fd].open_offset,len);
 	}	
 	file_table[fd].open_offset+=len;
