233bd1ab9753af2ccbbde602b1cb1952868ee617
tracer-ics2019 <tracer@njuics.org>
2019-12-07 23:51:13 +0800

    >  run 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  23:51:13 up 6 days, 14:38,  4 users,  load average: 0.46, 0.32, 0.19 d8ccf8bf6984d8b6a2662e26facdfe9c3ad82f2b

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -82,32 +82,44 @@ int fs_close(int fd)
     return 0;
 }
 
-size_t fs_read(int fd, void* buf, size_t len)
-{
-    Finfo* cur_file = &file_table[fd];
-    if (!cur_file->read) {
-        if (cur_file->open_offset > cur_file->size) return 0;
-        if (len > cur_file->size - cur_file->open_offset)
-            len = cur_file->size - cur_file->open_offset;
-        ramdisk_read(buf, cur_file->disk_offset + cur_file->open_offset, len);
-    } else
-        len = cur_file->read(buf, cur_file->disk_offset + cur_file->open_offset, len);
-    cur_file->open_offset += len;
-    return len;
+size_t fs_read(int fd, void *buf, size_t len){
+  
+  size_t size = file_table[fd].size; 
+  intptr_t disk_offset = file_table[fd].disk_offset;
+  intptr_t open_offset = file_table[fd].open_offset;
+  intptr_t remain = size - open_offset;
+  intptr_t _start = disk_offset + open_offset;
+
+  if(!file_table[fd].read)
+  {
+    if(open_offset > size) return 0;
+    else if(len > remain) len = remain;
+    ramdisk_read(buf, _start,len);
+  }
+  else 
+    len = file_table[fd].read(buf, _start, len);
+  file_table[fd].open_offset += len;
+  return len; 
 }
 
-size_t fs_write(int fd, const void* buf, size_t len)
-{
-    Finfo* cur_file = &file_table[fd];
-    if (!cur_file->write) {
-        if (cur_file->open_offset > cur_file->size) return 0;
-        if (len > cur_file->size - cur_file->open_offset)
-            len = cur_file->size - cur_file->open_offset;
-        ramdisk_write(buf, cur_file->disk_offset + cur_file->open_offset, len);
-    } else
-        len = cur_file->write(buf, cur_file->disk_offset + cur_file->open_offset, len);
-    cur_file->open_offset += len;
-    return len;
+size_t fs_write(int fd, void *buf, size_t len){
+  
+  size_t size = file_table[fd].size; 
+  intptr_t disk_offset = file_table[fd].disk_offset;
+  intptr_t open_offset = file_table[fd].open_offset;
+  intptr_t remain = size - open_offset;
+  intptr_t _start = disk_offset + open_offset;
+
+  if(!file_table[fd].write)
+  {
+    if(open_offset > size) return 0;
+    else if(len > remain) len = remain;
+    ramdisk_write(buf, _start,len);
+  }
+  else 
+    len = file_table[fd].write(buf, _start, len);
+  file_table[fd].open_offset += len;
+  return len; 
 }
 
 size_t fs_lseek(int fd, size_t offset, int whence)
