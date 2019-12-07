a2e023f9455f644e3d8472d0f9cb1acb78fcb478
tracer-ics2019 <tracer@njuics.org>
2019-12-07 22:34:30 +0800

    >  run 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  22:34:30 up 6 days, 13:22,  4 users,  load average: 0.35, 0.60, 0.62 fce426fc70b52cbdc0b7802a9799cfb34e540285

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -17,219 +17,152 @@ int printf(const char *fmt, ...) {
   return ans;
 }
 
-// int vsprintf(char* out, const char* fmt, va_list ap)
-// {
-//     /* version 1.0 on Oct 18th, 2019, only %s and %d supported*/
-//     /* version 2.0 on Oct 23th, 2019, width and zero flag supported*/
-//     char* outptr = out;
-//     char* s;
-//     int d;
-//     unsigned u;
-//     char tmp_num[20];
+int vsprintf(char* out, const char* fmt, va_list ap)
+{
+    /* version 1.0 on Oct 18th, 2019, only %s and %d supported*/
+    /* version 2.0 on Oct 23th, 2019, width and zero flag supported*/
+    /* version 2.1 on Dec 7th, 2019, from ht, will be changed later*/
+    char* outptr = out;
+    char* s;
+    int d;
+    unsigned u;
+    char tmp_num[20];
 
-//     int d_negative_no_zero_padded_flag = 0;  //negative number with no zero_padded
+    int d_negative_no_zero_padded_flag = 0;  //negative number with no zero_padded
 
-//     int zero_padded = 0;
-//     int in_format = 0;
-//     int width_now = 0;
-//     size_t i;
-//     while (*fmt != '\0') {
-//         if (in_format) {
-//             switch (*fmt) {
-//                 case 's':  //string
-//                     s = va_arg(ap, char*);
-//                     int count_s = width_now - strlen(s);
-//                     while (count_s-- > 0) {
-//                         if (zero_padded)
-//                             *outptr++ = '0';
-//                         else
-//                             *outptr++ = ' ';
-//                     }
-//                     while (*s != '\0') *outptr++ = *s++;
-//                     in_format = 0;
-//                     break;
-//                 case 'd':  //integer
-//                     d = va_arg(ap, int);
-//                     if (d < 0) {
-//                         d = -d;
-//                         if (zero_padded) {
-//                             *outptr++ = '-';
-//                             --width_now;
-//                         } else
-//                             d_negative_no_zero_padded_flag = 1;
-//                     } else if (d == 0) {
-//                         *outptr++ = '0';
-//                         --width_now;
-//                         while (width_now-- > 0) *outptr++ = '0';
-//                     }
-//                     for (i = 1; d; i++, d /= 10)
-//                         tmp_num[i] = (d % 10) + '0';
-//                     if (d_negative_no_zero_padded_flag)
-//                         tmp_num[i++] = '-';
-//                     int count_d = width_now - (i - 1);
-//                     while (count_d-- > 0) {
-//                         if (zero_padded)
-//                             *outptr++ = '0';
-//                         else
-//                             *outptr++ = ' ';
-//                     }
-//                     for (i--; i; i--)
-//                         *outptr++ = tmp_num[i];
-//                     in_format = 0;
-//                     break;
-//                 case 'u':  //unsigned
-//                     u = va_arg(ap, unsigned);
-//                     if (u == 0) {
-//                         *outptr++ = '0';
-//                         --width_now;
-//                         while (width_now-- > 0) *outptr++ = '0';
-//                     }
-//                     for (i = 1; u; i++, u /= 10)
-//                         tmp_num[i] = (u % 10) + '0';
-//                     int count_u = width_now - (i - 1);
-//                     while (count_u-- > 0) {
-//                         if (zero_padded)
-//                             *outptr++ = '0';
-//                         else
-//                             *outptr++ = ' ';
-//                     }
-//                     for (i--; i; i--)
-//                         *outptr++ = tmp_num[i];
-//                     in_format = 0;
-//                     break;
-//                 case 'x':  //hex
-//                 case 'p':  //pointer
-//                     if (*fmt == 'p') {
-//                         *outptr++ = '0';
-//                         *outptr++ = 'x';
-//                     }
-//                     u = va_arg(ap, unsigned);
-//                     if (u == 0) {
-//                         *outptr++ = '0';
-//                         --width_now;
-//                         while (width_now-- > 0) *outptr++ = '0';
-//                     }
-//                     for (i = 1; u; i++, u /= 16) {
-//                         if (u % 16 <= 9) {
-//                             tmp_num[i] = (u % 16) + '0';
-//                         } else {
-//                             tmp_num[i] = (u % 16) + 'a' - 10;
-//                         }
-//                     }
-//                     int count_x = width_now - (i - 1);
-//                     while (count_x-- > 0) {
-//                         if (zero_padded)
-//                             *outptr++ = '0';
-//                         else
-//                             *outptr++ = ' ';
-//                     }
-//                     for (i--; i; i--)
-//                         *outptr++ = tmp_num[i];
-//                     in_format = 0;
-//                     break;
-//                 case '0':
-//                     if (*(fmt - 1) == '%')
-//                         zero_padded = 1;
-//                     else
-//                         width_now *= 10;
-//                     break;
-//                 case '1':
-//                 case '2':
-//                 case '3':
-//                 case '4':
-//                 case '5':
-//                 case '6':
-//                 case '7':
-//                 case '8':
-//                 case '9':
-//                     width_now = 10 * width_now + *fmt - '0';
-//                     break;
-//                 default: assert(0);
-//             }
-//             ++fmt;
-//         } else if (*fmt == '%') {
-//             in_format = 1;
-//             width_now = 0;
-//             zero_padded = 0;
-//             ++fmt;
-//         } else {
-//             *outptr = *fmt;
-//             ++fmt;
-//             ++outptr;
-//         }
-//     }
-//     *outptr = '\0';
-//     return outptr - out;
-// }
-
-char _buf[256]={};
-char str[256]={};
-
-char *myitoa(int val, char *_buf, int base){
-    memset(_buf,0,sizeof(_buf));
-    memset(str,0,sizeof(str));
-    char *p = _buf;
-    unsigned digval;
-    int neg = 0;
-    if(val < 0 && base == 10){
-        neg = 1;
-        val = (unsigned long)(-(long)val);
+    int zero_padded = 0;
+    int in_format = 0;
+    int width_now = 0;
+    size_t i;
+    while (*fmt != '\0') {
+        if (in_format) {
+            switch (*fmt) {
+                case 's':  //string
+                    s = va_arg(ap, char*);
+                    int count_s = width_now - strlen(s);
+                    while (count_s-- > 0) {
+                        if (zero_padded)
+                            *outptr++ = '0';
+                        else
+                            *outptr++ = ' ';
+                    }
+                    while (*s != '\0') *outptr++ = *s++;
+                    in_format = 0;
+                    break;
+                case 'd':  //integer
+                    d = va_arg(ap, int);
+                    if (d < 0) {
+                        d = -d;
+                        if (zero_padded) {
+                            *outptr++ = '-';
+                            --width_now;
+                        } else
+                            d_negative_no_zero_padded_flag = 1;
+                    } else if (d == 0) {
+                        *outptr++ = '0';
+                        --width_now;
+                        while (width_now-- > 0) *outptr++ = '0';
+                    }
+                    for (i = 1; d; i++, d /= 10)
+                        tmp_num[i] = (d % 10) + '0';
+                    if (d_negative_no_zero_padded_flag)
+                        tmp_num[i++] = '-';
+                    int count_d = width_now - (i - 1);
+                    while (count_d-- > 0) {
+                        if (zero_padded)
+                            *outptr++ = '0';
+                        else
+                            *outptr++ = ' ';
+                    }
+                    for (i--; i; i--)
+                        *outptr++ = tmp_num[i];
+                    in_format = 0;
+                    break;
+                case 'u':  //unsigned
+                    u = va_arg(ap, unsigned);
+                    if (u == 0) {
+                        *outptr++ = '0';
+                        --width_now;
+                        while (width_now-- > 0) *outptr++ = '0';
+                    }
+                    for (i = 1; u; i++, u /= 10)
+                        tmp_num[i] = (u % 10) + '0';
+                    int count_u = width_now - (i - 1);
+                    while (count_u-- > 0) {
+                        if (zero_padded)
+                            *outptr++ = '0';
+                        else
+                            *outptr++ = ' ';
+                    }
+                    for (i--; i; i--)
+                        *outptr++ = tmp_num[i];
+                    in_format = 0;
+                    break;
+                case 'x':  //hex
+                case 'p':  //pointer
+                    if (*fmt == 'p') {
+                        *outptr++ = '0';
+                        *outptr++ = 'x';
+                    }
+                    u = va_arg(ap, unsigned);
+                    if (u == 0) {
+                        *outptr++ = '0';
+                        --width_now;
+                        while (width_now-- > 0) *outptr++ = '0';
+                    }
+                    for (i = 1; u; i++, u /= 16) {
+                        if (u % 16 <= 9) {
+                            tmp_num[i] = (u % 16) + '0';
+                        } else {
+                            tmp_num[i] = (u % 16) + 'a' - 10;
+                        }
+                    }
+                    int count_x = width_now - (i - 1);
+                    while (count_x-- > 0) {
+                        if (zero_padded)
+                            *outptr++ = '0';
+                        else
+                            *outptr++ = ' ';
+                    }
+                    for (i--; i; i--)
+                        *outptr++ = tmp_num[i];
+                    in_format = 0;
+                    break;
+                case '0':
+                    if (*(fmt - 1) == '%')
+                        zero_padded = 1;
+                    else
+                        width_now *= 10;
+                    break;
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    width_now = 10 * width_now + *fmt - '0';
+                    break;
+                default: assert(0);
+            }
+            ++fmt;
+        } else if (*fmt == '%') {
+            in_format = 1;
+            width_now = 0;
+            zero_padded = 0;
+            ++fmt;
+        } else {
+            *outptr = *fmt;
+            ++fmt;
+            ++outptr;
+        }
     }
-
-    do{
-        digval = (unsigned) (val % base);
-        val /= base;
-
-        if(digval > 9)
-            *p++ = (char)(digval -10 +'a');
-        else
-            *p++ = (char)(digval + '0'); //didn't support hex number. Keep working.
-    }while(val > 0);
-    if(neg) *p++ = '-';
-    *p++ = '\0';
-    int len = strlen(_buf);
-    for (int i = 0; i < len; i++)
-    {
-        str[i] = _buf[len-1-i]; 
-    }
-
-    return str;
+    *outptr = '\0';
+    return outptr - out;
 }
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  char *s;
-  char *start = out;
-  int string_length=0;
-  for (; *fmt; fmt++)
-  {
-    if( *fmt != '%' ){
-      *out++ = *fmt;
-      continue;
-    }
-    fmt++; //skip '%'
-    switch(*fmt){
-      case 's': s = va_arg(ap, char *);
-                out = strcpy(out, s);  
-                string_length=strlen(s);
-                out+=string_length;
-                continue;
 
-      case 'd': myitoa(va_arg(ap, int),_buf,10);
-                //printf("_buf1: %s\n",str);  
-                out = strcpy(out, str);
-                //printf("_buf: %s\n",str);  
-                string_length=strlen(str);
-                out+=string_length;
-                continue;
-      case 'x': myitoa(va_arg(ap,int),_buf,16);
-                out = strcpy(out,str);
-                string_length=strlen(str);
-                out+=string_length;
-                continue;
-    }
-  }
-  int cnt = out - start;
-  return cnt;
-}
 int sprintf(char *out, const char *fmt, ...) {
   int ans;
   va_list ap;
