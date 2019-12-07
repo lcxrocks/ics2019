01b19f4aa0d9ffc9b186f42735197ef1edb35cee
tracer-ics2019 <tracer@njuics.org>
2019-12-07 16:22:10 +0800

    >  run 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  16:22:10 up 6 days,  7:20,  4 users,  load average: 0.52, 0.24, 0.14 ef4fafbb34661026d96a71fda926e40e576da1ed

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -65,8 +65,8 @@ int fs_open(const char *pathname, int flags, int mode)
       printf("hahhaha\n");
       file_table[i].open_offset = 0;
       printf("Finish load %s\n",pathname);
-      return i;
       
+      return i;
     }
     printf("comparing for the %d time\n", i);
   }
