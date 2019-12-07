a10507f87b78ed1a66384ca39074dd13403d3a9e
tracer-ics2019 <tracer@njuics.org>
2019-12-07 21:22:51 +0800

    >  compile 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:22:51 up 6 days, 12:17,  4 users,  load average: 0.56, 0.38, 0.30 1d3a8dbad20a9bda27a6520aa88394b2feb25eb

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -84,6 +84,15 @@ void init_difftest(char *ref_so_file, long img_size) {
 static void checkregs(CPU_state *ref, vaddr_t pc) {
   if (!isa_difftest_checkregs(ref, pc)) {
     extern void isa_reg_display(void);
+  printf("eax_QEMU 0x%8x %8u\n", ref->eax,ref->eax);
+  printf("ecx_QEMU 0x%8x %8u\n", ref->ecx, ref->ecx);
+  printf("edx_QEMU 0x%8x %8u\n", ref->edx,ref->edx);
+  printf("ebx_QEMU 0x%8x %8u\n", ref->ebx,ref->ebx);
+  printf("esp_QEMU 0x%8x %8u\n", ref->esp,ref->esp);
+  printf("ebp_QEMU 0x%8x %8u\n", ref->ebp,ref->ebp);
+  printf("esi_QEMU 0x%8x %8u\n", ref->esi,ref->esi);
+  printf("edi_QEMU 0x%8x %8u\n", ref->edi,ref->edi);
+  printf("eip_QEMU 0x%8x %8u\n", ref->pc,ref->pc);
     isa_reg_display();
     nemu_state.state = NEMU_ABORT;
     nemu_state.halt_pc = pc;
