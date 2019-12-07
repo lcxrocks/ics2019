f6bd6291c61805f097900a76e1e159bf0d5f274e
tracer-ics2019 <tracer@njuics.org>
2019-12-07 21:10:06 +0800

    >  compile 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:10:06 up 6 days, 12:04,  4 users,  load average: 0.20, 0.31, 0.31 7af7db6a389c336824f632403a9f6b45b8ea9ce

--- nemu/src/isa/x86/init.c
+++ nemu/src/isa/x86/init.c
@@ -16,6 +16,7 @@ const long isa_default_img_size = sizeof(isa_default_img);
 static void restart() {
   /* Set the initial program counter. */
   cpu.pc = PC_START;
+  cpu.cs = 8;
   cpu.init = 0x2;
 }
 
