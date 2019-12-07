1e9a5dcce084b71986fd4646e929bc7e3d6265af
tracer-ics2019 <tracer@njuics.org>
2019-12-07 11:32:11 +0800

    >  run 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  11:32:11 up 6 days,  4:51,  4 users,  load average: 0.36, 0.16, 0.06 e0adfd6d57c9cd81708a52ee604d608a4a529f92

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -57,13 +57,14 @@ void init_fs() {
 
 int fs_open(const char *pathname, int flags, int mode)
 {
-  //Log("pathname :%s\n",pathname);
+  Log("pathname :%s\n",pathname);
   for (size_t i = 0; i < NR_FILES; i++)
   {
     if(strcmp(file_table[i].name, pathname)==0)
     { 
       file_table[i].open_offset = 0;
       return i;
+      Log("Finish load %s\n",pathname);
     }
   }
   panic("File not found!\n");
