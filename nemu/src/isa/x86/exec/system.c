407f6fd2753dd398e80b8ea25baa44166069cb0d
tracer-ics2019 <tracer@njuics.org>
2020-01-10 21:22:06 +0800

    >  compile 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  21:22:06 up 2 days, 13:19,  5 users,  load average: 0.51, 0.39, 0.29 a93862f6eedcb865874a7bf3102d72d992e16c04

--- nemu/src/isa/x86/exec/system.c
+++ nemu/src/isa/x86/exec/system.c
@@ -35,8 +35,6 @@ make_EHelper(mov_r2cr) {
   case 3: 
     //this is cr3 register
     cpu.cr3.val = id_src->val;
-    printf("%x\n",cpu.cr3.val);
-    assert(0);
     break;
   default:
     printf("should not reach here --> mov_r2cr()\n");
