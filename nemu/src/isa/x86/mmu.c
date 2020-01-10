638f0c5cc528f58ee028768587932c8a516c4b15
tracer-ics2019 <tracer@njuics.org>
2020-01-11 00:05:50 +0800

    >  compile 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  00:05:50 up 2 days, 15:55,  6 users,  load average: 0.16, 0.18, 0.15 c122d5845979c60582804ef480c6ae402613b50f

--- nemu/src/isa/x86/mmu.c
+++ nemu/src/isa/x86/mmu.c
@@ -1,6 +1,6 @@
 #include "nemu.h"
 
-paddr_t page_translate(vaddr addr, bool wren){
+paddr_t page_translate(vaddr_t addr, bool wren){
   PDE pde;
   PTE pte;
   if (cpu.cr0.paging)
@@ -9,19 +9,19 @@ paddr_t page_translate(vaddr addr, bool wren){
     uint32_t page_idx = (addr >> 12) & 0x3ff; //10 bits of PAGE 页表索引
     uint32_t offset = addr & PAGE_MASK; // 12 bits of PAGE offset 页内地址
     
-    paddr_t page_dir = dir *4 + cpu.cr3.page_directory_base << 12;
+    paddr_t page_dir = dir *4 + (cpu.cr3.page_directory_base << 12);
     pde.val = paddr_read(page_dir, 4); //get the page_directory 页表基地址
     assert(pde.present); //if pde.present == 0, then the page is not usable
     pde.accessed = 1; // “由MMU在进行地址转换时将该位置1”
 
-    paddr_t page_addr = page_idx *4 + pde.page_frame << 12;
+    paddr_t page_addr = page_idx *4 + (pde.page_frame << 12);
     pte.val = paddr_read(page_addr, 4);
     assert(pte.present);
-    assert(!((!pte.read_write)&wren)) //can't write when (R/W = 0 && wren = 1)
+    assert(!((!pte.read_write)&wren)); //can't write when (R/W = 0 && wren = 1)
     pte.accessed = 1;
     pte.dirty = wren ? 1 : pte.dirty; // “由MMU在进行写操作的时候将该位置1”
 
-    return  ((pte.page_frame <<12) | offset)
+    return  ((pte.page_frame <<12) | offset);
   }
   else 
     return addr;
@@ -49,7 +49,7 @@ void isa_vaddr_write(vaddr_t addr, uint32_t data, int len) {
   else 
   {
     paddr_t paddr = page_translate(addr, true);
-    paddr_write(paddr,len);
+    paddr_write(paddr,data,len);
   }
 }
 
