f0c273b4610decb2fa4270e6296dba92f41b15ce
181240035-Liu Chunxu <181240035@smai.nju.edu.cn>
2020-01-11 15:22:31 +0800

    #12 Retun to pa3 to check an idea

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,10 +3,10 @@ _Context* do_syscall(_Context *c);
 _Context* schedule(_Context* prev);
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
-    case _EVENT_YIELD: Log("haha,hit _EVENT_YIELD\n"); 
+    case _EVENT_YIELD:  
       return schedule(c);
     break;
-    case _EVENT_SYSCALL: Log("EVENT_SYSCALL\n");
+    case _EVENT_SYSCALL: 
       return do_syscall(c); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
