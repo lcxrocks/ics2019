8b305e23f4622f560d8be5d5c3f4f214fd0744ea
tracer-ics2019 <tracer@njuics.org>
2020-01-11 15:09:13 +0800

    >  run 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  15:09:13 up 3 days,  3:58,  6 users,  load average: 0.36, 0.27, 0.24 ab23a1bdc648e21cf28e0ab4d7103ac2bcd1adb

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,7 @@ _Context* do_syscall(_Context *c);
 _Context* schedule(_Context* prev);
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
-    case _EVENT_YIELD: Log("haha,hit _EVENT_YIELD\n"); 
+    case _EVENT_YIELD: //Log("haha,hit _EVENT_YIELD\n"); 
     return schedule(c);
     break;
     case _EVENT_SYSCALL: return do_syscall(c); break;
