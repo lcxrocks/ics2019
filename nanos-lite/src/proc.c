92bf2e2bcf40809f3fca40f03cd121345a8a60ba
tracer-ics2019 <tracer@njuics.org>
2020-01-11 09:35:47 +0800

    >  run 181240035 chauncey Linux debian 4.19.0-5-amd64 #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) x86_64 GNU/Linux  09:35:47 up 2 days, 23:04,  6 users,  load average: 0.80, 0.40, 0.31 9bcea86836ca48ad4e77a0470a8b51c398327b

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -25,13 +25,13 @@ void hello_fun(void* arg)
 
 void init_proc()
 {
-    context_uload(&pcb[1],"/bin/init");
-    context_kload(&pcb[0],(void *)hello_fun);
+    //context_uload(&pcb[1],"/bin/init");
+    //context_kload(&pcb[0],(void *)hello_fun);
     switch_boot_pcb();
 
     Log("Initializing processes...");
 
-    //naive_uload(NULL, "/bin/init");
+    naive_uload(NULL, "/bin/pal");
 }
 
 _Context* schedule(_Context* prev)
